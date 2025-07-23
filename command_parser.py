41f46418a8099c815294e5d961e73806dbc6bfb4
IronMan1405 <daksheshnankani1405@gmail.com>
2025-07-23 21:03:43 +0530

    added open commands

--- command_parser.py
+++ command_parser.py
@@ -1,3 +1,4 @@
+import os, random, webbrowser
 from datetime import datetime
 
 def greet():
@@ -12,6 +13,18 @@ def get_date():
 def exit_jarvis():
     return "Goodbye!"
 
+def open_youtube():
+    webbrowser.open("https://www.youtube.com")
+    return "Opening Youtube"
+
+def open_google():
+    webbrowser.open("https://www.google.com")
+    return "Opening Google"
+
+def open_code():
+    os.system("open -a 'Visual Studio Code'")
+    return "Opening VS Code"
+
 def unknown():
     return "Sorry! I didn't catch that."
 
@@ -21,7 +34,10 @@ commands = {
     "time" : get_time,
     "date" : get_date,
     "exit" : exit_jarvis,
-    "quit" : exit_jarvis
+    "quit" : exit_jarvis, 
+    "youtube": open_youtube,
+    "google": open_google,
+    "code": open_code,
 }
 
 def handleCommand(cmd: str) -> str:
