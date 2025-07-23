d8696aaf46d9a4a8b38f40e4c9ad85472bcbb1b3
IronMan1405 <daksheshnankani1405@gmail.com>
2025-07-23 20:10:47 +0530

    added to sr and tts

---
+++ speech_recognition.py
@@ -0,0 +1,27 @@
+from vosk import Model, KaldiRecognizer
+import pyaudio
+import json
+
+def get_voice_command():
+    model = Model("models/vosk-model-small-en-us-0.15")
+    recogniser = KaldiRecognizer(model, 16000)
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+
+    stream.start_stream()
+
+    print("Listening...")
+
+    while True:
+        data = stream.read(4000, exception_on_overflow=False)
+
+        if recogniser.AcceptWaveForm(data):
+            result = json.loads(recogniser.Result())
+            cmd = result.get()
+
+            if cmd:
+                stream.stop_stream()
+                stream.close()
+                p.terminate()
+                return cmd
